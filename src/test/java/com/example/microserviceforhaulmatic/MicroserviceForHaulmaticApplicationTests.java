e537fdaabf8b0f9d5532dcace6d7f6c2d8f35ab4
vipisanan <vipisanan1@gmail.com>
2020-06-12 12:09:10 +0530

    first commit

---
+++ src/test/java/com/example/microserviceforhaulmatic/MicroserviceForHaulmaticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.microserviceforhaulmatic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceForHaulmaticApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
