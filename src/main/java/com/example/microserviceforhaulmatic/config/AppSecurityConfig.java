27607612bdd62bf644e3ddda505b10af1a1bb139
vipisanan <vipisanan1@gmail.com>
2020-08-14 23:22:26 +0530

    HttpSecurity working with inMemory

--- src/main/java/com/example/microserviceforhaulmatic/config/AppSecurityConfig.java
+++ src/main/java/com/example/microserviceforhaulmatic/config/AppSecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.microserviceforhaulmatic.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -21,7 +22,17 @@ public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
     protected UserDetailsService userDetailsService() {
         List<UserDetails> users = new ArrayList<>();
 
-        users.add(User.withDefaultPasswordEncoder().username("vipi").password("1234").roles("USER").build());
+        users.add(User.withDefaultPasswordEncoder().username("user").password("1234").roles("USER").build());
+        users.add(User.withDefaultPasswordEncoder().username("admin").password("1234").roles("ADMIN").build());
         return new InMemoryUserDetailsManager(users);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/admin").hasAnyRole("ADMIN")
+                .antMatchers("/user").hasAnyRole("USER")
+                .antMatchers("/").permitAll()
+                .and().formLogin();
     }
 }
