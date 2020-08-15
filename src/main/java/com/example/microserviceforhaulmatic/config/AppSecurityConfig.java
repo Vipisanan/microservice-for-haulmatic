1bcac0ad6e7fcd37eafd177ae1989c7908551c8d
vipisanan <vipisanan1@gmail.com>
2020-08-15 10:54:25 +0530

    fetch data with UserDetailsService hardCord

--- src/main/java/com/example/microserviceforhaulmatic/config/AppSecurityConfig.java
+++ src/main/java/com/example/microserviceforhaulmatic/config/AppSecurityConfig.java
@@ -1,38 +1,41 @@
 package com.example.microserviceforhaulmatic.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-
-import java.util.ArrayList;
-import java.util.List;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Bean
-    @Override
-    protected UserDetailsService userDetailsService() {
-        List<UserDetails> users = new ArrayList<>();
+    @Autowired
+    UserDetailsService userDetailsService;
 
-        users.add(User.withDefaultPasswordEncoder().username("user").password("1234").roles("USER").build());
-        users.add(User.withDefaultPasswordEncoder().username("admin").password("1234").roles("ADMIN").build());
-        return new InMemoryUserDetailsManager(users);
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService);
+
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/admin").hasAnyRole("ADMIN")
-                .antMatchers("/user").hasAnyRole("USER")
+                .antMatchers("/user","/user/**").hasAnyRole("ADMIN" , "USER")
                 .antMatchers("/").permitAll()
                 .and().formLogin();
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
 }
